ac104210f678c4f980377a3a4a61ee463bd66507
syahrul208 <95618936+syahrul208@users.noreply.github.com>
2021-12-30 19:37:58 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const model = require('./sdk/model.js'); //predict
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5057562264:AAHLUz5s6G-8vDMtapgD6l1Y4Z2i5su7OlM'
+const token = '5032864819:AAHupqAGzGolmVV4QcDfMIddH6ghfSzBVbc'
 const bot = new TelegramBot(token, {polling: true});
 
 state=0;
